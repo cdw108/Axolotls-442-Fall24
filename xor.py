aa45f773fd5861ee2c58fbe7130356fcd782442a
Jacob Yee <Jacobdy18@hotmail.com>
2024-10-29 22:39:27 -0500

    it works

--- xor.py
+++ xor.py
@@ -2,6 +2,7 @@ import sys
 
 buffer_size = 4096
 
+#xor the whole thing
 def xor(data, key):
     result = bytearray()
     for data, key in zip(data, key):
@@ -10,15 +11,16 @@ def xor(data, key):
 
 
 #read the key
-with open("key", "rb") as key_file:
+with open("key2-1", "rb") as key_file:
     key = bytearray(key_file.read())
 
 #read the text
 binary_data = sys.stdin.buffer.read()
-
+if(len(binary_data) != len(key)):
+    sys.exit()
 #xorthem
 result = xor(binary_data, key)
 
 
-
+#print the result
 sys.stdout.buffer.write(result)
